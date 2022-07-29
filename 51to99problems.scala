ab0c93ddac92a5a37b9f0ece3de2ed47b629d121
alvinvoo <alvin.voo@gmail.com>
2022-07-29 23:22:08 +0800

    until problem 55. not quite solved yet

--- 51to99problems.scala
+++ 51to99problems.scala
@@ -1,3 +1,4 @@
+import scala.math.*
 // problem 54A
 // check whether a list represent a binary tree
 // scala> isTree(List(1, List(2, null, null)))
@@ -16,6 +17,67 @@ def isTree[A](l: List[A]): Boolean =
 
 // problem 55
 // better have a generic Tree class 
+class Tree[A](var data: A, var left: Tree[A], var right: Tree[A]):
+  def updateDataInPlace(data: A) =
+    this.data = data
 
+  def swapInPlace() = 
+    val tt = left
+    this.left = right
+    this.right = tt
 
+  def swapInPlaceWithDirs(l: List[String]): Boolean =
+    var tt = this
+    for ll <- l do 
+      ll match
+      case "left" | "l" => tt = tt.left
+      case "right" | "r" => tt = tt.right
+    // if this is a node leaf, then nothing to swap
+    // return false
+    // println(s"tt is ${tt}")
+    if tt == null || tt.isNodeLeaf()
+    then 
+      false
+    else 
+      tt.swapInPlace()
+      true
 
+  def isNodeLeaf(): Boolean =
+    left == null && right == null
+    
+  override def toString = s"|${left}<-${data}->${right}|"
+end Tree
+
+object Tree:
+ def clone[A](x: Tree[A]): Tree[A] =
+   if x == null then null
+   else if x.isNodeLeaf() then Tree(x.data, null, null)
+   else Tree(x.data, Tree.clone(x.left), Tree.clone(x.right))
+
+def cBalTreeOne[A](data: A, n: Int): Tree[A] =
+  if n < 1 then null
+  else if n == 1 then Tree(data, null, null)
+  else
+    val nn = n - 1
+    var f1 = floor; var f2 = ceil
+    Tree(data, cBalTreeOne(data, f1(nn/2.0).toInt), cBalTreeOne(data, f2(nn/2.0).toInt))
+
+// the number of variations would be 2^(#longest valid depth)
+def cBalTreePrintRest[A](tog: Tree[A], dir: List[String] = List()): Unit =
+    val tclone = Tree.clone(tog)
+    if tclone.swapInPlaceWithDirs(dir) then
+      // if previous dir encounters a node leaf then
+      // nothing to print/proceed
+      println(tclone)
+      cBalTreePrintRest(tog, dir :+ "l")
+      cBalTreePrintRest(tclone, dir :+ "l")
+      cBalTreePrintRest(tog, dir :+ "r")
+      cBalTreePrintRest(tclone, dir :+ "r")
+  
+// have duplicates - coz the clone flipped might attempt back the flip
+def cBalTreePrint[A](data: A, n: Int): Unit =
+  val t = cBalTreeOne(data, n)
+  println(s"Original tree: ${t}")
+  cBalTreePrintRest(t)
+
+  
